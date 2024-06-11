ac0b61f9243b25c13af580d99dc5ee1fbd9fd03a
soyung sim <simsy1024@gmail.com>
2024-06-11 18:08:49 +0900

    Initial Commit

---
+++ MovieSearchApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieSearchApp
+//
+//  Created by 심소영 on 6/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
